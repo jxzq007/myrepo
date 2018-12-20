4fe1a7ab2550c40f181b3d763e3a6a1d7e4ad411
zhangqing <jxzq007@gmail.com>
2018-12-20 21:26:15 +0800

    创建Emp，看看CLASS文件是否会上传到Github

---
+++ TestGit/src/org/zhangqing/vo/Emp.java
@@ -0,0 +1,69 @@
+package org.zhangqing.vo;
+
+import java.io.Serializable;
+import java.util.Date;
+
+@SuppressWarnings("serial")
+public class Emp implements Serializable {
+	private Integer empno;
+	private String ename;
+	private String job;
+	private Date hiredate;
+	private Double salary;
+	private Double comm;
+
+	public Integer getEmpno() {
+		return empno;
+	}
+
+	public void setEmpno(Integer empno) {
+		this.empno = empno;
+	}
+
+	public String getEname() {
+		return ename;
+	}
+
+	public void setEname(String ename) {
+		this.ename = ename;
+	}
+
+	public String getJob() {
+		return job;
+	}
+
+	public void setJob(String job) {
+		this.job = job;
+	}
+
+	public Date getHiredate() {
+		return hiredate;
+	}
+
+	public void setHiredate(Date hiredate) {
+		this.hiredate = hiredate;
+	}
+
+	public Double getSalary() {
+		return salary;
+	}
+
+	public void setSalary(Double salary) {
+		this.salary = salary;
+	}
+
+	public Double getComm() {
+		return comm;
+	}
+
+	public void setComm(Double comm) {
+		this.comm = comm;
+	}
+
+	@Override
+	public String toString() {
+		return "Emp [empno=" + empno + ", ename=" + ename + ", job=" + job + ", hiredate=" + hiredate + ", salary="
+				+ salary + ", comm=" + comm + "]";
+	}
+
+}
