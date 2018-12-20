11cc32e62518d198da94463059dec199bef3656c
zhangqing <jxzq007@gmail.com>
2018-12-20 21:32:03 +0800

    编写了DAO

---
+++ TestGit/src/org/zhangqing/dao/IEmpDAO.java
@@ -0,0 +1,13 @@
+package org.zhangqing.dao;
+
+import java.util.Set;
+
+import org.zhangqing.vo.Emp;
+
+public interface IEmpDAO {
+	public boolean doCreate(Emp vo) throws Exception;
+
+	public boolean doUpdate(Emp vo) throws Exception;
+
+	public boolean doRemove(Set<Integer> ids) throws Exception;
+}
